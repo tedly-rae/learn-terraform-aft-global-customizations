f67fc8f8cc98cd5a67d725de08b7e49062ee1c67
Tan Ly <tientan0912@gmail.com>
2023-12-27 17:12:04 +0700

    create a new ec2 ssm role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,76 +1,76 @@
-# resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-#   name = "ec2_ssm_profile"
-#   role = aws_iam_role.ec2_ssm_role.name
-# }
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "ec2_ssm_profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
 
-# resource "aws_iam_role" "ec2_ssm_role" {
-#   name = "ec2_ssm_role"
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "ec2_ssm_role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy" "ec2_ssm_role_policy" {
-#   name = "ec2_ssm_role_policy"
-#   role = aws_iam_role.ec2_ssm_role.id
+resource "aws_iam_role_policy" "ec2_ssm_role_policy" {
+  name = "ec2_ssm_role_policy"
+  role = aws_iam_role.ec2_ssm_role.id
 
-#   policy = jsonencode({
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "Action": [
-#                 "ssm:DescribeAssociation",
-#                 "ssm:GetDeployablePatchSnapshotForInstance",
-#                 "ssm:GetDocument",
-#                 "ssm:DescribeDocument",
-#                 "ssm:GetManifest",
-#                 "ssm:GetParameter",
-#                 "ssm:GetParameters",
-#                 "ssm:ListAssociations",
-#                 "ssm:ListInstanceAssociations",
-#                 "ssm:PutInventory",
-#                 "ssm:PutComplianceItems",
-#                 "ssm:PutConfigurePackageResult",
-#                 "ssm:UpdateAssociationStatus",
-#                 "ssm:UpdateInstanceAssociationStatus",
-#                 "ssm:UpdateInstanceInformation"
-#             ],
-#             "Resource": "*"
-#         },
-#         {
-#             "Effect": "Allow",
-#             "Action": [
-#                 "ssmmessages:CreateControlChannel",
-#                 "ssmmessages:CreateDataChannel",
-#                 "ssmmessages:OpenControlChannel",
-#                 "ssmmessages:OpenDataChannel"
-#             ],
-#             "Resource": "*"
-#         },
-#         {
-#             "Effect": "Allow",
-#             "Action": [
-#                 "ec2messages:AcknowledgeMessage",
-#                 "ec2messages:DeleteMessage",
-#                 "ec2messages:FailMessage",
-#                 "ec2messages:GetEndpoint",
-#                 "ec2messages:GetMessages",
-#                 "ec2messages:SendReply"
-#             ],
-#             "Resource": "*"
-#         }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:DescribeDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        }
+    ]
+  })
+}
